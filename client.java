1ab85d31e240f03e7476a4b9f327d2caeace757b
Divyajot Singh Mankan <63389007+divyajot98@users.noreply.github.com>
2020-09-30 19:30:46 +0530

    Add files via upload

---
+++ client.java
@@ -0,0 +1,13 @@
+package GameDevelopment;
+
+public class client {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+TicTacToe t=new TicTacToe();
+	}
+
+}
